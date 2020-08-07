12486d6e9525d2d746ae2ff42d94f054887dc7fe
mahmudul01 <smhasan021@gmail.com>
2020-08-07 19:47:54 +0600

    Simple stopwatch start and stop funtioality implemented

---
+++ OOP.Practice/Stopwatch/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Threading;
+
+namespace Stopwatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stopwatch stopwatch1 = new Stopwatch();
+            Stopwatch stopwatch2 = new Stopwatch();
+
+            stopwatch1.Start();
+
+            stopwatch2.Start();
+
+            Thread.Sleep(2000);
+
+            stopwatch1.Stop();
+
+            stopwatch2.Stop();
+
+            stopwatch1.Start();
+
+            Thread.Sleep(3000);
+
+            stopwatch1.Stop();
+
+            TimeSpan duration1 = stopwatch1.Duration;
+
+            TimeSpan duration2 = stopwatch2.Duration;
+
+            Console.WriteLine("Stopwatch 1: " + duration1.TotalSeconds);
+            Console.WriteLine("Stopwatch 2: " + duration2.TotalSeconds);
+        }
+    }
+}
---
+++ OOP.Practice/Stopwatch/Stopwatch.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Stopwatch
+{
+    public class Stopwatch
+    {
+        private DateTime startTime;
+        private bool isStarted = false;
+        public TimeSpan Duration { get; private set; }
+
+        public void Start()
+        {
+            if (isStarted == true)
+                throw new InvalidOperationException("Stopwatch already started");
+
+            isStarted = true;
+            startTime = DateTime.Now;
+        }
+
+        public void Stop()
+        {
+            isStarted = false;
+
+            var timeElapsed = DateTime.Now - startTime;
+
+            Duration = Duration.Add(timeElapsed);
+        }
+
+    }
+}
