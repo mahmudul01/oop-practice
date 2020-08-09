1b7678362d1990de3187c01ea0da53a2039f9859
mahmudul01 <smhasan021@gmail.com>
2020-08-09 13:08:15 +0600

    Simple elevator operation implemented

---
+++ OOP.Practice/Elevator/Elevator.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Elevator
+{
+    public class Elevator
+    {
+        private int MaximumNumber = 8;
+        public bool IsOpeded { get; private set; } = false;
+        public int TotalPersons { get; private set; }
+
+        public void Open()
+        {
+            if (IsOpeded == true)
+            {
+                throw new InvalidOperationException("Lift is already opened.");
+            }
+
+            Console.WriteLine("Opening the door.");
+
+            IsOpeded = true;
+        }
+        public void Enter(int number)
+        {
+
+            if (!IsOpeded)
+            {
+                throw new InvalidOperationException("Lift is closed.");
+            }
+
+            if ((TotalPersons + number) > MaximumNumber)
+            {
+                throw new InvalidOperationException("Overloaded.");
+            }
+
+            TotalPersons = TotalPersons + number;
+
+            Console.WriteLine($"Entering person in the lift: { number}");
+            Console.WriteLine($"Total person in the lift: {TotalPersons}");
+        }
+        public void Close()
+        {
+            //if (!isOpeded)
+            if (IsOpeded == false)
+            {
+                throw new InvalidOperationException("Lift is already closed.");
+            }
+            Console.WriteLine("Closing the door.");
+            IsOpeded = false;
+        }
+        public void Exit(int number)
+        {
+            if (IsOpeded == false)
+            {
+                Console.WriteLine("Lift is already closed.");
+            }
+            TotalPersons = TotalPersons - number;
+            Console.WriteLine($"Exiting peorson:{number}");
+            Console.WriteLine($"Total person in the lift:  {TotalPersons}");
+            IsOpeded = true;
+        }
+        public void Emergency()
+        {
+            Console.WriteLine("=============");
+            Console.WriteLine("Emergecy call");
+            Open();
+            Exit(TotalPersons);
+            Close();
+        }
+    }
+}
---
+++ OOP.Practice/Elevator/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Elevator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var lift = new Elevator();
+            lift.Open();
+
+            lift.Enter(4);
+            lift.Close();
+
+            lift.Open();
+            lift.Exit(2);
+            lift.Close();
+            lift.Emergency();
+
+            Console.WriteLine($"Total person in the lift: {lift.TotalPersons}");
+            var doorStatus = lift.IsOpeded ? "Open" : "Closed";
+            Console.WriteLine($"Door Status: {doorStatus}");
+        }
+    }
+}
